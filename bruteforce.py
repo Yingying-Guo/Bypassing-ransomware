90241355b03adf11068184b5612129407ca4d48b
s3860867-Yingyinig-Guo <guo1337993624@126.com>
2022-11-16 17:32:25 +1100

    Complete

--- bruteforce.py
+++ bruteforce.py
@@ -6,10 +6,12 @@ Bruteforce starter template
 from zipfile import ZipFile
 
 # Use a method to attempt to extract the zip file with a given password
-# def attempt_extract(zf_handle, password):
-#     
-#
-#
+def attempt_extract(zf_handle, password):
+    try:
+        zf_handle.extractall(pwd=password)
+        return True
+    except:
+        return False
 
 def main():
     print("[+] Beginning bruteforce ")
@@ -17,12 +19,17 @@ def main():
         with open('rockyou.txt', 'rb') as f:
             # Write your logic here...
             # Iterate through password entries in rockyou.txt
+            for p in f:
+                password = p.strip()
+                # Attempt to extract the zip file using each password
+                # Handle correct password extract versus incorrect password attempt)
+                if attempt_extract(zf, password):
+                    print("[+] Correct password: %s" % password)
+                    exit(0)
+                else:
+                    print("[-] Incorrect password: %s" % password)
 
-            # Attempt to extract the zip file using each password
-
-            # Handle correct password extract versus incorrect password attempt)
-
-    #print("[+] Password not found in list")
+    print("[+] Password not found in list")
 
 if __name__ == "__main__":
     main()
